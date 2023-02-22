1aa336693bea1a4c491ea15d94a39e1674ed4749
lukasjoc <45790416+lukasjoc@users.noreply.github.com>
2023-02-22 17:34:57 +0100

    remove duplicate textwidth

--- lua/prelude.lua
+++ lua/prelude.lua
@@ -2,7 +2,7 @@ vim.opt.encoding = "utf-8"
 vim.opt.ttyfast = true 
 vim.opt.title = true
 vim.opt.exrc = true
-vim.g.autosource_conf_names = { '.vimrc', '.exrc', '.exrc.lua' }
+vim.g.autosource_conf_names = { ".vimrc", ".exrc", ".exrc.lua" }
 vim.opt.paste = false
 vim.opt.swapfile = false
 -- INFO:  choosing m here because <Space> as a leader seems to be slow with telescope and such
@@ -23,13 +23,9 @@ vim.o.textwidth = 0
 vim.o.wrapmargin = 0
 vim.wo.linebreak = false
 vim.wo.list = false
-
 vim.opt.scrolloff = 21
-
 vim.opt.incsearch = true
 vim.opt.hlsearch = true
-
-vim.opt.textwidth = 92
 vim.opt.laststatus = 0
 
 vim.opt.colorcolumn = "92"
@@ -37,7 +33,7 @@ vim.opt.termguicolors = true
 
 vim.filetype.add({
     extension = {
-        tor        = 'todoreadme',
-        todoreadme = 'todoreadme',
+        tor        = "todoreadme",
+        todoreadme = "todoreadme",
     },
 })
